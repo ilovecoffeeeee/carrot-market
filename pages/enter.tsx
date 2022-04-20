b2bfc1f14727edf11041b7a60b47e0859db41b18
sun_loves_Q <conconxel@gmail.com>
2022-04-20 23:25:50 +0900

    upload Items page

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -35,12 +35,12 @@ export default function Enter() {
             </div>
           </div>
           <form className="flex flex-col mt-8">
-            <label className="text-sm font-medium text-gray-700">
+            <label htmlFor="input" className="text-sm font-medium text-gray-700">
               {method === "email" ? "Email address" : null}
               {method === "phone" ? "Phone number" : null}
             </label>
             <div className="mt-1">
-              {method === "email" ? <input type="email"  className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500" required /> : null}
+              {method === "email" ? <input id="input" type="email"  className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500" required /> : null}
               {method === "phone" ? (
                 <div className="flex rounded-md shadow-sm">
                   <span className="
@@ -57,7 +57,7 @@ export default function Enter() {
                   >
                     +82
                   </span>
-                  <input type="number"
+                  <input id="input" type="number"
                     className="
                     appearance-none
                     w-full px-3 py-2 
