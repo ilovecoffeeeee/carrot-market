d8c772cdad413c3e505247ebfdea8750f11a7190
sun_loves_Q <conconxel@gmail.com>
2022-05-11 02:39:42 +0900

    Authentication
    - Accounts Logic
    - Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,12 +1,65 @@
+import client from "@libs/server/client";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/server/client"
+import withHandler from "@libs/server/withHandler";
+import { prisma } from "@prisma/client";
 
-export default async function handler (
-    req:NextApiRequest, res:NextApiResponse
-){
-    if(req.method !== "POST"){
-        res.status(401).end();
-    }
-    console.log(req.body);
-    res.json({ok:true});
+async function handler (req:NextApiRequest, res:NextApiResponse){
+    const { phone, email } = req.body;
+    const user = phone ? {phone: +phone} : {email}
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
+    const token = await client.token.create({
+        data: {
+            payload,
+            user: {
+                connectOrCreate: {
+                    where : {
+                        ...user
+                    },
+                    create: {
+                        name: "Anonymous",
+                        ...user
+                    },
+                }
+            }
+        },
+    });
+    console.log(token)
+    // if (email){
+    //     user = await client.user.findUnique({
+    //         where:{
+    //             email,
+    //         }
+    //     });
+    //     if(user) console.log("found it.")
+    //     if(!user){
+    //         console.log("Did not find. Will create.");
+    //         user = await client.user.create({
+    //             data: {
+    //                 name:"Anonymous",
+    //                 email,
+    //             },
+    //         });
+    //     }
+    // }
+    // console.log(user)
+    // if (phone){
+    //     user = await client.user.findUnique({
+    //         where:{
+    //             phone: +phone,
+    //         }
+    //     });
+    //     if(user) console.log("found it.")
+    //     if(!user){
+    //         console.log("Did not find. Will create.");
+    //         user = await client.user.create({
+    //             data: {
+    //                 name:"Anonymous",
+    //                 phone: +phone,
+    //             },
+    //         });
+    //     }
+    // }
+    return res.status(202).end();
 }
+
+export default withHandler("POST", handler);
