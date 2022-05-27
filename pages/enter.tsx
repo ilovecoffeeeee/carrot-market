af8718af1c682f2e5976a9abc1213c8323bd50b2
sun_loves_Q <conconxel@gmail.com>
2022-05-27 13:49:39 +0900

    token

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -11,9 +11,22 @@ interface EnterForm {
   phone?: string;
 }
 
+interface TokenForm {
+  token: string;
+  
+}
+
+interface MutaionResult {
+  ok:boolean;
+}
+
 const Enter: NextPage = () => {
-  const [enter, { loading, data, error }] = useMutation("/api/users/enter");
+  const [enter, { loading, data, error }] = 
+    useMutation<MutaionResult>("/api/users/enter");
+  const [confirmToken, { loading:tokenLoading, data:tokenData }] =
+    useMutation<MutaionResult>("/api/users/confirm");
   const { register, handleSubmit, reset } = useForm<EnterForm>();
+  const { register:tokenRegister, handleSubmit:tokenHandleSubmit } = useForm<TokenForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
   const onEmailClick = () => {
     reset();
@@ -27,11 +40,37 @@ const Enter: NextPage = () => {
     if (loading) return;
     enter(validForm);
   };
+  const onTokenValid = (validForm:TokenForm) => {
+    if (tokenLoading) return;
+    confirmToken(validForm);
+  }
+
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
       <div className="mt-12">
-        {data?.ok ? null : (<>
+        {data?.ok ? 
+          (
+           <form
+           onSubmit={tokenHandleSubmit(onTokenValid)}
+           className="flex flex-col mt-8 space-y-4"
+         >
+           
+             <Input
+               register={tokenRegister("token", {
+                 required: true,
+               })}
+               name="token"
+               label="Confirmation Token"
+               type="number"
+               required
+             />
+           
+           
+             <Button text={tokenLoading ? "Loading" : "Confirm Token"} />
+           
+         </form>
+          ) : (<>
           <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
           <div className="grid border-b  w-full mt-8 grid-cols-2 ">
