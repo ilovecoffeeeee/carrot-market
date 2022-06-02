79fd27d7c6d0554ecbfda82f1eeb6fb124379a3f
sun_loves_Q <conconxel@gmail.com>
2022-06-03 02:06:13 +0900

    Start to making Authorization

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -5,6 +5,8 @@ import Button from "@components/button";
 import Input from "@components/input";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface EnterForm {
   email?: string;
@@ -44,7 +46,12 @@ const Enter: NextPage = () => {
     if (tokenLoading) return;
     confirmToken(validForm);
   }
-
+  const router = useRouter();
+  useEffect(() => {
+    if(tokenData?.ok){
+      router.push("/")
+    }
+  }, [tokenData, router])
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
