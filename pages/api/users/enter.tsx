b4b00bae8999eba874f0d5832b4d81ab1fd7cfd0
sun_loves_Q <conconxel@gmail.com>
2022-06-07 22:07:02 +0900

    authorization is done

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,9 +6,13 @@ import withHandler, { ResponseType } from "@libs/server/withHandler";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
+
+async function handler (
+    req:NextApiRequest,
+    res:NextApiResponse<ResponseType>
+) {
     const { phone, email } = req.body;
-    const user = phone ? {phone: +phone} : email ? {email} : null;
+    const user = phone ? {phone} : email ? {email} : null;
     if(!user) return res.status(400).json({ ok: false });
     const payload = Math.floor(100000 + Math.random() * 900000) + "";
     const token = await client.token.create({
@@ -39,7 +43,11 @@ async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
     }
     return res.json({
         ok:true,
-    })
+    });
 }
 
-export default withHandler("POST", handler);
+export default withHandler({
+    method:"POST",
+    handler,
+    isPrivate: false,
+});
