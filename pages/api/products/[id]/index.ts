3dd1315b82abe04297a8d60d02992183d8e962de
sun_loves_Q <conconxel@gmail.com>
2022-06-09 01:39:55 +0900

    Products page is done

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -5,7 +5,7 @@ import { withApiSession } from "@libs/server/withSession";
 
 
 async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
-    const { id } = req.query;
+    const { query: {id}, session : {user} } = req;
     const product = await client.product.findUnique({
         where: {
             id: +id.toString(),
@@ -20,7 +20,33 @@ async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
             },
         }
     })
-    res.json({ok:true, product,})
+    const terms = product?.name.split(" ").map(word => ({
+        name: {
+            contains: word,
+        },
+    }));
+    const relatedProducts = await client.product.findMany({
+        where: {
+            OR: terms,
+            AND: {
+                id: {
+                    not: product?.id,
+                }
+            }
+        }
+    })
+    const isLiked = Boolean(
+        await client.fav.findFirst({
+            where: {
+                productId: product?.id,
+                userId: user?.id,
+            },
+            select: {
+                id: true,
+            }
+        })
+    );
+    res.json({ok:true, product, isLiked, relatedProducts})
 }
 
 export default withApiSession(withHandler({
