74160a16c49b154404772e3afc0288859132326f
sun_loves_Q <conconxel@gmail.com>
2022-06-17 02:43:47 +0900

    community page is done

---
+++ libs/client/useCoords.ts
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+interface UseCoordState {
+    longitude: number | null;
+    latitude: number | null;
+}
+
+export default function useCoords(){
+    const [coords, setCoords] = useState<UseCoordState>({latitude: null, longitude: null});
+    const onSuccess = ({coords:{latitude, longitude}}: GeolocationPosition) => {
+        setCoords({latitude, longitude});
+    }
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(onSuccess);
+    },[])
+    return coords;
+}
