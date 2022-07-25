e2f4448f93994a49b3e6c721fe3e56c02aab1d1c
sun_loves_Q <conconxel@gmail.com>
2022-07-26 02:10:12 +0900

    try to make infinite-scroll

---
+++ pages/api/streams/index.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+
+async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
+    const {
+        query: { page },
+        session : {user},
+        body: {
+            name, price, description
+        },
+    } = req;
+    const take = 10;
+    const skip = (+page -1) * 10
+    if(req.method === "POST") {
+        const stream = await client.stream.create({
+            data: {
+                name,
+                price,
+                description,
+                user: {
+                    connect : {
+                        id: user?.id,
+                    },
+                },
+            },
+        });
+        res.json({ok:true, stream});
+    } else if(req.method === "GET") {
+        const streams = await client.stream.findMany({
+            take,
+            skip,
+    });
+        res.json({ok:true, streams});
+    }
+    
+}
+
+export default withApiSession(withHandler({
+    methods: ["GET", "POST"],
+    handler,
+}));
