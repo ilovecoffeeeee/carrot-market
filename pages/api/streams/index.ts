b5dbc10ce520072ae111f0791baa00c62204fdae
sun_loves_Q <conconxel@gmail.com>
2022-07-27 08:30:39 +0900

    setAvatarPreview

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -6,14 +6,11 @@ import { withApiSession } from "@libs/server/withSession";
 
 async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
     const {
-        query: { page },
         session : {user},
         body: {
             name, price, description
         },
     } = req;
-    const take = 10;
-    const skip = (+page -1) * 10
     if(req.method === "POST") {
         const stream = await client.stream.create({
             data: {
@@ -30,8 +27,8 @@ async function handler (req:NextApiRequest, res:NextApiResponse<ResponseType>){
         res.json({ok:true, stream});
     } else if(req.method === "GET") {
         const streams = await client.stream.findMany({
-            take,
-            skip,
+            take: 10,
+            skip: 20,
     });
         res.json({ok:true, streams});
     }
